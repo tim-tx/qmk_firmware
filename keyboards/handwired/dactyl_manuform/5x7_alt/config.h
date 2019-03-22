a6774e9809a653b2f1ae9b87565f517b82179c46
Tim Jones <tjones01@gmail.com>
2019-03-22 09:13:49 -0400

    Update dactyl_manuform 5x7_alt layout and add tsj-colemak keymap.

--- keyboards/handwired/dactyl_manuform/5x7_alt/config.h
+++ keyboards/handwired/dactyl_manuform/5x7_alt/config.h
@@ -28,6 +28,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_ROWS 12
 #define MATRIX_COLS 7
 
+#define TAPPING_TERM 200
+#define IGNORE_MOD_TAP_INTERRUPT
+
 // wiring of each half
 #define MATRIX_ROW_PINS { D4, C6, D7, E6, B4, B5 }
 #define MATRIX_COL_PINS { F5, F6, F7, B1, B3, B2, B6 }
--- keyboards/handwired/dactyl_manuform/5x7_alt/keymaps/tsj-colemak/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x7_alt/keymaps/tsj-colemak/keymap.c
@@ -24,11 +24,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,------------------------------------------------.           ,------------------------------------------------.
  * |   =  |   1  |   2  |   3  |   4  |   5  |  L1  |           |  L1  |   6  |   7  |   8  |   9  |   0  |   -  |
  * |------+------+------+------+------+------+------|           |------+------+------+------+------+------+------|
- * | Del  |   Q  |   W  |   E  |   R  |   T  | OS(1)|           | OS(1)|   Y  |   U  |   I  |   O  |   P  |   \  |
+ * | Del  |   Q  |   W  |   F  |   P  |   G  | OS(1)|           | OS(1)|   J  |   L  |   U  |   Y  |   ;  |   \  |
  * |------+------+------+------+------+------+------|           |------|------+------+------+------+------+------|
- * | BkSp |   A  |   S  |   D  |   F  |   G  |OSM(S)|           |OSM(S)|   H  |   J  |   K  |   L  |   ;  |'/Cmd |
+ * | BkSp |   A  |   R  |   S  |   T  |   D  |OSM(S)|           |OSM(S)|   H  |   N  |   E  |   I  |   O  |'/Cmd |
  * |------+------+------+------+------+------+------|           |------|------+------+------+------+------+------|
- * |(/LSft|Z/Ctrl| X/Alt| C/Gui|   V  |   B  | caps |           | caps |   N  |   M  | ,/Gui| ./Alt|//Ctrl|)/RSft|
+ * |(/LSft|Z/Ctrl| X/Alt| C/Gui|   V  |   B  | caps |           | caps |   K  |   M  | ,/Gui| ./Alt|//Ctrl|)/RSft|
  * `------+------+------+------+------+-------------'           `-------------+------+------+------+------+------'
  *               | Left | Right|                                                     |  Up  | Down |
  *               `-------------'                                                     `-------------'
@@ -45,8 +45,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT_5x7_alt(
 		    // left hand
 		    KC_EQL,         KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           TG(SYMB),
-		    KC_DELT,        KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           OSL(SYMB),
-		    KC_BSPC,        KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           OSM(MOD_LSFT),
+		    KC_DELT,        KC_Q,           KC_W,           KC_F,           KC_P,           KC_G,           OSL(SYMB),
+		    KC_BSPC,        KC_A,           KC_R,           KC_S,           KC_T,           KC_D,           OSM(MOD_LSFT),
 		    KC_LSPO,	    LCTL_T(KC_Z),   LALT_T(KC_X),   LGUI_T(KC_C),   KC_V,           KC_B,	    KC_CAPS,
 					 	    KC_LEFT,        KC_RGHT,
                          		            KC_TAB,         KC_SPC,
@@ -54,9 +54,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		                                    KC_END,         KC_HOME,
 		    // right hand
 		    TG(SYMB),       KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_MINS,
-                    OSL(SYMB),      KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLS,
-                    OSM(MOD_LSFT),  KC_H,           KC_J,           KC_K,           KC_L,           KC_SCLN,        KC_QUOT, 
-                    KC_CAPS,        KC_N,           KC_M,           RGUI_T(KC_COMM),RALT_T(KC_DOT), RCTL_T(KC_SLSH),KC_RSPC,
+                    OSL(SYMB),      KC_J,           KC_L,           KC_U,           KC_Y,           KC_SCLN,        KC_BSLS,
+                    OSM(MOD_LSFT),  KC_H,           KC_N,           KC_E,           KC_I,           KC_O,           KC_QUOT, 
+                    KC_CAPS,        KC_K,           KC_M,           RGUI_T(KC_COMM),RALT_T(KC_DOT), RCTL_T(KC_SLSH),KC_RSPC,
                                                                     KC_UP,          KC_DOWN,
                                                                     KC_ENT,         KC_BSPC, 
 		                                                    KC_ESC,         OSM(MOD_LALT),
